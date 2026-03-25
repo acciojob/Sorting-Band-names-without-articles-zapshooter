c0236a793af1d3383d8bcb62c20abd25454bb099
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-03-25 23:39:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-//your code here
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Acacia Strain',
+  'A Day to Remember',
+  'The Wonder Years',
+  'An Albatross',
+  'Bring Me the Horizon',
+  'Motionless in White'
+];
 
+function stripArticles(name) {
+  return name.replace(/^(a |an |the )/i, '').trim();
+}
+
+const sortedBands = bands.sort((a, b) =>
+  stripArticles(a).toLowerCase() > stripArticles(b).toLowerCase() ? 1 : -1
+);
+
+const bandList = document.querySelector('ul');
+bandList.id = 'bands';
+bandList.innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
